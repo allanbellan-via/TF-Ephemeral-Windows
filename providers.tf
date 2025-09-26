f2eb697467a6d33ea445a2172fe84ced3de3a0e0
Allan Bellan <allanbellan92@gmail.com>
2025-09-26 18:03:06 -0300

    Setup: ajuste provider

--- providers.tf
+++ providers.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "oci" {
   auth  = "InstancePrincipal"
   # escolha 1: declare a região aqui
